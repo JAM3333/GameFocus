a2ae1cf8d327c7797acc795d7ee88412c7336e9f
ZEWAAS <simon.wallner@student.htldornbirn.at>
2024-11-15 16:33:44 +0100

    topbar

--- GameFocus/src/pages/index.vue
+++ GameFocus/src/pages/index.vue
@@ -1,7 +1,14 @@
 <template>
   <HelloWorld />
+  <topbar />
 </template>
 
 <script setup>
   //
 </script>
+
+<style>
+.v-application--wrap {
+  overflow: visible; /* Ändern Sie overflow auf visible */
+}
+</style>
