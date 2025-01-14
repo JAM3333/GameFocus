a573ba08e780dd4bd94e82a63b90327e3017d390
KptnFishy <nils.miessgang@gmail.com>
2025-01-14 08:50:19 +0100

    Api calls on Spotlight item

--- GameFocus/src/javascript/GameFunctions.js
+++ GameFocus/src/javascript/GameFunctions.js
@@ -25,28 +25,37 @@ export async function GetPrices(games) {
   }
 }
 export async function FetchDeals(limit) {
-  axios
-    .get(
-      `https://api.isthereanydeal.com/deals/v2?key=${import.meta.env.VITE_API_KEY}&limit=16`
-    )
-    .then((response) => {
-      const items = response.data.list;
-      let saleItems = items.map((item) => ({
-        title: item.title,
-        gameId: item.id,
-        dealPrice: item.deal.price.amount.toFixed(2),
-        regularPrice: item.deal.regular.amount.toFixed(2),
-        discount: item.deal.cut,
-        shop: item.deal.shop.name,
-        url: item.deal.url,
-        drm: item.deal.drm.map((drm) => drm.name).join(", "),
-        platform: item.deal.platforms.map((platform) => platform.name).join(", "),
-      }));
-      console.log(saleItems)
-      return saleItems;
-    })
-    .catch((error) => {
-      console.error("Failed to fetch sale items:", error);
-      return [];
-    });
+  try {
+    const response = await axios.get(
+      `https://api.isthereanydeal.com/deals/v2?key=${import.meta.env.VITE_API_KEY}&limit=${limit}`
+    );
+    const items = response.data.list;
+    const saleItems = items.map((item) => ({
+      title: item.title,
+      gameId: item.id,
+      dealPrice: item.deal.price.amount.toFixed(2),
+      regularPrice: item.deal.regular.amount.toFixed(2),
+      discount: item.deal.cut,
+      shop: item.deal.shop.name,
+      url: item.deal.url,
+      drm: item.deal.drm.map((drm) => drm.name).join(", "),
+      platform: item.deal.platforms.map((platform) => platform.name).join(", "),
+    }));
+    return saleItems;
+  } catch (error) {
+    console.error("Failed to fetch sale items:", error);
+    return [];
+  }
+}
+export async function FetchGameInfo(gameId) {
+  try {
+    const response = await axios.get(
+      `https://api.isthereanydeal.com/games/info/v2?key=${import.meta.env.VITE_API_KEY}&id=${gameId}`
+    );
+    const gameInfo = response.data; // Access game-specific data
+    return gameInfo || {};
+  } catch (error) {
+    console.error(`Error fetching game info for gameId: ${gameId}`, error);
+    return {};
+  }
 }
